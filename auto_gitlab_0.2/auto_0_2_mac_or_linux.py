3a9d6e97eda0dc32b3132cce456b500e7018117f
한재희 <gkswogml23@naver.com>
2020-09-16 00:58:02 +0900

    Start with ver 0.2

--- auto_gitlab_0.2/auto_0_2_mac_or_linux.py
+++ auto_gitlab_0.2/auto_0_2_mac_or_linux.py
@@ -3,20 +3,25 @@ import time
 
 """
 ************************** PLEASE README ******************************
-usage
-repo_name : don't touch
-private_token : input your private_token
-user_name : input your user_name
+
+Auto GitLab ver 0.1 for Mac or Linux
+
+Usage
+
+private_token : Input your private_token
+user_name : Input your user_name
 work_space = input your local git repository path
+
 """
 
-#repo_name = 'hw_' + time.strftime('%y%m%d', time.localtime(time.time()))
-repo_name = 'hw_200915'
-private_token = 'm522bRJaYpBv_BXiUGZ3'
-user_name = 'gkswogml23'
-work_space = '/Users/hindsight/git/'
+repo_name = 'hw_' + time.strftime('%y%m%d', time.localtime(time.time()))
 
+private_token = 'Private Token' # Input your Private Token
+user_name = 'username' # Input your Username ex) lab.ssafy.com/kimssafy <-- kimssafy
+work_space = 'your workspace path' # ex) '/Users/kimssafy/ssafy/work_java'
 print(repo_name)
+
+# init repository and commit
 os.chdir(work_space)
 os.system('mkdir ' + repo_name)
 os.chdir('./'+ repo_name)
@@ -24,10 +29,16 @@ os.system('git init')
 os.system('touch README')
 os.system('git add .')
 os.system('git commit -m \'created by gitlab-auto HJH\'')
-os.system('git push --set-upstream https://lab.ssafy.com/gkswogml23/' + repo_name + '.git master')
 
+# git push
+git_push = 'git push --set-upstream https://lab.ssafy.com/' + user_name + '/' + repo_name + '.git master'
+os.system(git_push)
+
+# add Member ssafy7
 member_add = 'curl --request POST --header "PRIVATE-TOKEN: ' + private_token + '" --data "user_id=487&access_level=30" "https://lab.ssafy.com/api/v4/projects/' + user_name + '%2F' + repo_name + '/members"'
 os.system(member_add)
+
+# check Members. If you can check ssafy7 in lines, good
 member_chk = 'curl --request GET --header "PRIVATE-TOKEN: ' + private_token + '" \'https://lab.ssafy.com/api/v4/projects/' + user_name + '%2F' + repo_name +'/members\''
 os.system(member_chk)
 
